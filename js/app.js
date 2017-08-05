b3772b71eab9ad23417b0f60731fef4838361a1c
Martin Splitt <martin@geekonaut.de>
2017-08-06 01:19:31 +0200

    Adds JSFiddle support

--- js/app.js
+++ js/app.js
@@ -24,17 +24,35 @@ var sidebar = document.querySelector('aside')
 var aframeEntity = document.querySelector('a-entity[io3d-furniture]').components['io3d-furniture']
 var snippet = document.querySelector('code')
 var idContainer = document.getElementById('product-id')
+var nameContainer = document.getElementById('product-name')
+var fiddle = document.querySelector('textarea')
+var fiddleTitle = document.querySelector('input[name="title"]')
+var fiddleDesc = document.querySelector('input[name="description"]')
+
+var FIDDLE_SNIPPET = `
+<script src="https://aframe.io/releases/0.6.1/aframe.min.js"></script>
+<script src="https://3d.io/releases/3dio-js/1.x.x-beta/3dio.min.js"></script>
+<script src="https://cdn.rawgit.com/tizzle/aframe-orbit-controls-component/v0.1.12/dist/aframe-orbit-controls-component.min.js"></script>
+
+<a-scene>
+  <a-entity id="furniture" io3d-furniture="id:[[productId]]" position="0 0 -3"></a-entity>
+  <a-sky color="#ececec"></a-sky>
+  <a-entity camera position="0 1.6 0" orbit-controls="target:#furniture; autoRotate: true"></a-entity>
+</a-scene>`
 
 // Event handlers
+
+var apiErrorCount = 0;
 
 search.addEventListener('input', debounce(function searchFurniture() {
   resultsContainer.textContent = 'Searching...'
   IO3D.utils.services.call('Product.search', {
     searchQuery: {
-      query: `${search.value}*`
+      query: `isPublished:true ${search.value}*`
     },
     limit: 50
   }).then(function (results) {
+    apiErrorCount = 0
     resultsContainer.innerHTML = ''
     if(results.length === 0) {
       resultsContainer.textContent = 'Woah, no furniture found that matches your criteria'
@@ -46,11 +64,16 @@ search.addEventListener('input', debounce(function searchFurniture() {
       item.querySelector('.product-name').textContent = product.productDisplayName
       item.querySelector('.product-manufacturer').textContent = product.manufacturer
       item.children[0].dataset.productId = product.productResourceId
+      item.children[0].dataset.productName = product.productDisplayName
       resultsContainer.appendChild(item)
     })
   }).catch(function (err) {
     console.error('Error fetching furniture:', err)
-    searchFurniture()
+    if(++apiErrorCount < 3) {
+      searchFurniture()
+    } else {
+      resultsContainer.textContent = 'Whoops, that did not work, please try another query.'
+    }
   })
 }, 500))
 
@@ -64,14 +87,23 @@ resultsContainer.addEventListener('click', function (evt) {
     return
   }
 
-  var productId = productContainer.dataset.productId
+  var productId = productContainer.dataset.productId,
+      productName = productContainer.dataset.productName
 
-  sidebar.classList.add('menu-active')
+  idContainer.textContent = productId
+  nameContainer.textContent = productName
+
   aframeEntity.data.id = productId
   aframeEntity.update()
-  idContainer.textContent =productId
+
   snippet.textContent = `<a-scene>\n\t<a-entity io3d-furniture="id:${productId}"></a-entity>\n</a-scene>`
   Prism.highlightElement(snippet)
+
+  fiddle.value = FIDDLE_SNIPPET.replace('[[productId]]', productId)
+  fiddleTitle.value = `3d.io "${productName} in A-Frame"`
+  fiddleDesc.value = `Displays the 3d.io furniture piece "${productName}" in an A-Frame scene`
+
+  sidebar.classList.add('menu-active')
 })
 
 document.getElementById('back').addEventListener('click', function() {
